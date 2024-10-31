787d5751edd1018e694b6d9b7044d27e9519537a
진아영 <ay8160@daum.net>
2024-10-31 22:36:02 +0900

    feat: 로그인 스크린 변경
    Former-commit-id: e84fb4af0f49096d71d3028e619cfb457ec2a336

--- frontend/app/odd/lib/main.dart
+++ frontend/app/odd/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5c912cb3d77fa3b6e33bdc6d8622fe023025ebcb00bcb3e748bb32fe25036d2
-size 3589
+oid sha256:dd0ff023b2ce9640cc8defd2fe480fc0e7b631139ae25bc410b0dbd953c38e7b
+size 3680
