093a72f6fd9471d207fc6715b8cb10991bd76e03
진아영 <ay8160@daum.net>
2024-11-01 12:45:47 +0900

    feat: 제품 카테고리 리스트 추가
    Former-commit-id: 4e88f2b80d74054f2b95802756b955a231318cd7

--- frontend/app/odd/lib/modules/home/widgets/platform_content.dart
+++ frontend/app/odd/lib/modules/home/widgets/platform_content.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:779b15bc17d3152be9d9e8fe2965b35e6902f4956fccaf0b2fd2f4bcf9dbe443
-size 5020
+oid sha256:72b0b291e7414597b904dea398329b114fdc3427ef0039357c92c85b67e6c568
+size 6056
