5e316a9c8116f7841e64f19c2d6dbccba31dcba3
c4526 <c4526@naver.com>
2024-11-01 10:38:01 +0900

    feat: 선호 카테고리 추가
    Former-commit-id: 445458b5de4ea43b023afdd1dff570532a56dce0

--- frontend/app/odd/lib/modules/user/screens/signup_screen.dart
+++ frontend/app/odd/lib/modules/user/screens/signup_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa6df76af542e55cb1d3e6993ae2d578bccfff05a2a98976ff31b90752ba2e46
-size 5973
+oid sha256:6d91ae78d7f8a992fc17d2b3f1b597faab72152450827e58ffc0022d68dbb488
+size 6471
---
+++ frontend/app/odd/lib/modules/user/widgets/fav_category_modal.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:03312a64895188874097e2df7cbe571b89ade00191046c68b27be9b9e73c9612
+size 9644
