6132439cd8614cdd1e891105356bfc383f191d2b
c4526 <c4526@naver.com>
2024-11-01 17:11:49 +0900

    feat: 로그인, 회원가입 컨트롤러 분리
    Former-commit-id: b77cced4bca1e407ce4cefde62ab38ba17b4259b

--- frontend/app/odd/lib/main.dart
+++ frontend/app/odd/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4fc59f93da2ebad382f5772f72497c0acba93441ee32d1ffe653bfa568fed84
-size 3680
+oid sha256:ac1ac11f900063dd36f4652814e0a5261ba836cfc7af07facea8adbf81a1e0de
+size 4183
--- frontend/app/odd/lib/modules/user/screens/login_screen.dart
+++ frontend/app/odd/lib/modules/user/screens/login_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:405c662c87db42feda1ede8c0e13c098ebbd931580aad48ba59594b654485e34
-size 3172
+oid sha256:4b712080b84e12db7b048dde17ab5511c4992cc0ebd98e378bd0df9ebe1c4f3c
+size 3038
