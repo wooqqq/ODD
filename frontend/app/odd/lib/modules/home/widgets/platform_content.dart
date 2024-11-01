4f02423bf201c876318661d6d93445f1485aa8cc
진아영 <ay8160@daum.net>
2024-11-01 12:56:21 +0900

    feat: 네비게이션 삭제
    Former-commit-id: 958d62a1fcee4728142bccb516dd57399167b321

--- frontend/app/odd/lib/modules/home/widgets/platform_content.dart
+++ frontend/app/odd/lib/modules/home/widgets/platform_content.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72b0b291e7414597b904dea398329b114fdc3427ef0039357c92c85b67e6c568
-size 6056
+oid sha256:e8b7a74ea159bf5410d0dd932e5cf775ae47645043db9bc193f5d4381b2e002e
+size 5375
