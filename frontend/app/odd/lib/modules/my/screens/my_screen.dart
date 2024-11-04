cd45a46242b8d68b1544b43bbc824fc436d3c17d
c4526 <c4526@naver.com>
2024-11-04 14:29:07 +0900

    feat: 포인트 페이지 추가, 마이페이지 네비게이션 추가
    Former-commit-id: cad78a0074fad0ba0212f497b4fb8fe698a50554

--- frontend/app/odd/lib/modules/my/screens/my_screen.dart
+++ frontend/app/odd/lib/modules/my/screens/my_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:088ae49686b3401e45d0c5868ec7451896147c91f8706ec42a3701bc5ce5a964
-size 5814
+oid sha256:3ca6323a0e4fc1b20a41e09ef5089c174fe444c5249216bcdbab8d53b3f59666
+size 7620
