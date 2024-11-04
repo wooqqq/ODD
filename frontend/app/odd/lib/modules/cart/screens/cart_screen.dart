226cf4f13ff04a245bcc8b97ecf0391b49510cad
진아영 <ay8160@daum.net>
2024-11-04 09:34:59 +0900

    feat: 장바구니 삭제 추가
    Former-commit-id: c93760dcd788fce43627f961961b5a26f701480b

--- frontend/app/odd/lib/modules/cart/screens/cart_screen.dart
+++ frontend/app/odd/lib/modules/cart/screens/cart_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42dca6b446e88a0d3a295c48d299779ffb33ce1029f5dd28a577080c4de34c53
-size 2429
+oid sha256:6bc768e7d59e859de036b97d3e24e72b752ef6b2c8e572451fd0feb40ace9a40
+size 2625
--- frontend/app/odd/lib/modules/cart/widgets/cart_item.dart
+++ frontend/app/odd/lib/modules/cart/widgets/cart_item.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07ffee5eb052ff43f3e35fd13f7d8ae25d31538197679425c8ae7ce09e589284
-size 1881
+oid sha256:323c7d1a6865b5177e3b52d49b4b6a64b18fc5d97e4edfc2e5fafdb5da94faf9
+size 2182
