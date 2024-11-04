813271a5611e96ce6703d5280ed5054d1959605b
진아영 <ay8160@daum.net>
2024-11-04 15:07:27 +0900

    feat: 금액 포맷팅
    Former-commit-id: 438f044419f557f4d0b495a9b4a95592244263aa

--- frontend/app/odd/lib/modules/cart/widgets/cart_item.dart
+++ frontend/app/odd/lib/modules/cart/widgets/cart_item.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31beceb5b505c617d577fe4622c7e069f2313743e43bffd0da8c568dc8c467ac
-size 2467
+oid sha256:27968f67c55b5ff0869e40682db904f0239028a3eaf5429b74fc23693f1eb06e
+size 2586
