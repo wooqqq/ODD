7a33ea36315db209cf4bc8e94ea798718b69fe93
c4526 <c4526@naver.com>
2024-11-04 16:52:06 +0900

    feat: 검색바 위젯 추가, 검색 페이지 검색바만 추가
    Former-commit-id: c56f0da660899af232b2fc4a5650be35ea01031f

--- frontend/app/odd/lib/modules/search/screens/search_screen.dart
+++ frontend/app/odd/lib/modules/search/screens/search_screen.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:odd/modules/common/default_layout.dart';
+import 'package:odd/modules/search/widgets/searchbackbar.dart';
 
 class SearchScreen extends StatelessWidget {
-  SearchScreen({
-    Key? key
-  }) : super(key: key);
-
+  const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(child:
-      Text('search screen')
-    );
+    return const DefaultLayout(child: SearchBackBar());
   }
 }
