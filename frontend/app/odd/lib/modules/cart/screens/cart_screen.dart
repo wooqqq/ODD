73fb9d25be788a47eac426b5a8f5a055d33fbedd
진아영 <ay8160@daum.net>
2024-11-04 13:46:10 +0900

    singlechildscrollview 개별 적용
    Former-commit-id: 0e91b3fa8bcb87c5fcc512a958d8718f979cb90b

--- frontend/app/odd/lib/modules/cart/screens/cart_screen.dart
+++ frontend/app/odd/lib/modules/cart/screens/cart_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:756aa2f60b38a024d6918eaeaaebd1b9512fdd702bb182e071c4e548281c80e4
-size 4995
+oid sha256:dbcf03b0f9ca071bcba2faaefdfd9f4e0148ab0c4e300227abc598953360c2f6
+size 6387
