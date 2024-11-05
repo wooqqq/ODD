21101b66e9b5b089b008564e1ae8d840dda41006
이소민 <s.min.s9098@gmail.com>
2024-11-05 12:38:21 +0900

    feat: SwaggerConfig 배포
    Former-commit-id: f7f0db9fada2fd0bdd8585c2f75d66b799835aa0

--- backend/client/src/main/java/odd/client/config/SwaggerConfig.java
+++ backend/client/src/main/java/odd/client/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19718ae10ca438216110a4159fe94bf14a38ebfcd3c2b5abc02692c06d090728
-size 1907
+oid sha256:ab6c0e9c0a2727c7a4d810ca05c5e9b4a47f01479ca0962400e98c687fe72e9d
+size 1802
