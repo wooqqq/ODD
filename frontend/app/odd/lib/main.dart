1167a36bddcb60c47e85303bf02c535939944508
진아영 <ay8160@daum.net>
2024-11-05 10:04:53 +0900

    feat: 네브바 분리
    Former-commit-id: 4a202ba58b2cc2f3d2937e668d017f5d47741a79

--- frontend/app/odd/lib/main.dart
+++ frontend/app/odd/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9494665ae429afdeb0382cee694ff1afbd8ba1d03e43f36fd1a914eedb1b6b5
-size 4361
+oid sha256:ed26a075d9ee8864663e51c9d1665439e2b435764ebb065ba0f05d348527e63c
+size 1447
--- frontend/app/odd/lib/modules/my/screens/my_screen.dart
+++ frontend/app/odd/lib/modules/my/screens/my_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ca6323a0e4fc1b20a41e09ef5089c174fe444c5249216bcdbab8d53b3f59666
-size 7620
+oid sha256:d32a37c6b56463f8be435b368d1ded0f98271e173cf178d277642e38bbeb8f42
+size 7641
