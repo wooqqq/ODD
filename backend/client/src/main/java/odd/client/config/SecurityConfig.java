a72a9ef3187b96bddb96e11688459952e8f05f3c
이소민 <s.min.s9098@gmail.com>
2024-11-05 11:00:37 +0900

    feat: swagger config 수정
    Former-commit-id: 7a7c5091449083f64ffd66da195fafd716c1a431

--- backend/client/src/main/java/odd/client/config/SecurityConfig.java
+++ backend/client/src/main/java/odd/client/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55c2c344fb2788414ada0d425818f3d6a74d4101ac9af179ece64f8305e31d12
-size 2618
+oid sha256:28dac2f8673f21991bd1bb87b7bbc137982c1c2b135cfe7042e152f8e638c0ec
+size 2597
