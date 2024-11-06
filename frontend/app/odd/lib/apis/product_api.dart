9b265b4a79c65c114cc42c995d5b0ad0ddcbc3a2
진아영 <ay8160@daum.net>
2024-11-06 17:52:34 +0900

    feat: default layout 수정
    Former-commit-id: 59cf04af6972dfc4bf682946d11e9d18d7d0f201

--- frontend/app/odd/lib/apis/product_api.dart
+++ frontend/app/odd/lib/apis/product_api.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56ef810f763ff3031aea59daf6aa4ff1c009de012af06cd3bd35c7f97f60330a
-size 1139
+oid sha256:c083f9159e4f7226faba5e0117f032f0a78960d31089035143c500e261451ad3
+size 914
