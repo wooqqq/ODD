32a9ecfe930005e2fd65147f23598c9e2e4745fd
최승탁 <tmdxkr5@hanmail.net>
2024-11-06 16:34:06 +0900

    hotfix
    Former-commit-id: 09b8f533609ce2ab8cc9e8ac43d6a243883d3a3d

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5c948cc278c38227e8577cecd5a542bd15ae0b1c426c87a599a876c0e4075fbc
-size 584
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/recommend")
+async def root():
+    return {"message": "Welcome to the Repurchase Recommendation System"}
