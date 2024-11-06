d115366c970b954cfc90e7706bc01524374def63
최승탁 <tmdxkr5@hanmail.net>
2024-11-06 12:41:56 +0900

    fix: SwaggerConfig 수정
    Former-commit-id: a8da969ddf4687153b1ee91aff32dd5b19cfccb2

--- backend/client/src/main/java/odd/client/config/SwaggerConfig.java
+++ backend/client/src/main/java/odd/client/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab6c0e9c0a2727c7a4d810ca05c5e9b4a47f01479ca0962400e98c687fe72e9d
-size 1802
+oid sha256:baab47676955c5363ab8edff4a626f5eda93a216ca5f8ff519fa36b0ec7eedc9
+size 1682
