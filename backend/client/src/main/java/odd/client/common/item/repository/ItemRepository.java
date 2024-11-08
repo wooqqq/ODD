0cd4cff22806d98cfc60bc0ff8d8783f7d6e8bdf
최승탁 <tmdxkr5@hanmail.net>
2024-11-08 16:12:42 +0900

    feat: 상품 리스트 조회 API subcategory에 대해서 middle로 조회 추가
    Former-commit-id: 57acdeaf35ebd0a4affbb86648862639c2ca0419

--- backend/client/src/main/java/odd/client/common/item/repository/ItemRepository.java
+++ backend/client/src/main/java/odd/client/common/item/repository/ItemRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e70c73504f007cc74adc32f1d7bf47b2d4888a57bee8da0f32f3ffc964d04247
-size 1342
+oid sha256:86d53e6ac1614d7f6bd7fe21f3ebc110b613dab21a171181906fd2602be025ea
+size 2038
