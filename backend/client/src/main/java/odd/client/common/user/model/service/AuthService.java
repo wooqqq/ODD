b00c41d85650833c7bb26d6d3eada5e6f8b7fd68
이소민 <s.min.s9098@gmail.com>
2024-11-08 11:16:47 +0900

    feat: 포인트 조회 및 결제
    Former-commit-id: 50c2c5b841f4fc45b5de9d21d29dd83606549d8a

--- backend/client/src/main/java/odd/client/common/user/model/service/AuthService.java
+++ backend/client/src/main/java/odd/client/common/user/model/service/AuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:774e0930eee027b4905f9956f21ad161329d65bdb0ccedb4965a768961aab914
-size 2971
+oid sha256:4f629ea3f6ad7a54c1bbded88f4e75b3f1a29b6a23497854a2075c91de0efe53
+size 3360
