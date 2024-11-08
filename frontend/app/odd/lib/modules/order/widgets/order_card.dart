1e99919be4890b641fac15e53c9033d1bf6597b7
c4526 <c4526@naver.com>
2024-11-08 23:54:47 +0900

    feat: 주문상세 header UI & 네비게이션 설정
    Former-commit-id: 479e2f59ec51a2e20bf403215612b57e176bf5d5

--- frontend/app/odd/lib/modules/order/widgets/order_card.dart
+++ frontend/app/odd/lib/modules/order/widgets/order_card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e2ae4b4cf1e5ccf2c29e60f27e0fb7622d01f23de253ef44826071ee466c5ed
-size 4775
+oid sha256:84a1927806739a7a87ec6624d435cfee5af3711f26c715de38d717a76e587477
+size 4914
