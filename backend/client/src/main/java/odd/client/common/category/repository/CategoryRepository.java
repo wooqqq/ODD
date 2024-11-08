27e6903345d1563d63617f6c102587be61fe3e2f
최승탁 <tmdxkr5@hanmail.net>
2024-11-08 15:20:45 +0900

    feat: 상품 상세 조회 API
    Former-commit-id: 0f6369ce30b672f045fecb6b1c6eef0b16b3f54b

--- backend/client/src/main/java/odd/client/common/category/repository/CategoryRepository.java
+++ backend/client/src/main/java/odd/client/common/category/repository/CategoryRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:914e71a80eacdc3a53a1b904a3b83e221e85721221ad5e368dee5bc2dc97f92f
-size 669
+oid sha256:19da61e240468aff6170d8b184f947e0cfbfc8b3e3af91d1a7e0b40aa61ec150
+size 672
