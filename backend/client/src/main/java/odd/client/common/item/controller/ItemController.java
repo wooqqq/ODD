a5209753dfe0aa6995704c9d5927cff5a8d769f1
최승탁 <tmdxkr5@hanmail.net>
2024-11-09 16:13:43 +0900

    feat: 추천 상품 목록 조회 API 초기 설정
    Former-commit-id: e213c2922c0b1bdc6638f9c0b871d6468c78f58e

--- backend/client/src/main/java/odd/client/common/item/controller/ItemController.java
+++ backend/client/src/main/java/odd/client/common/item/controller/ItemController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89de2b6ff688fbc498acd7b6b9f5bdf559077d592ca3a5748ce00c35553f8bc2
-size 4891
+oid sha256:8b853c46cf3e5ca0639fe9ca77e146e65b8aadea117b11833c07c039d1eb1932
+size 7813
--- backend/client/src/main/java/odd/client/common/item/service/ItemService.java
+++ backend/client/src/main/java/odd/client/common/item/service/ItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1536cf64aa06277021c9c119949ea79b075d66a1a5951f412bb6e02473bb97c
-size 5063
+oid sha256:31b296beb53fe1066042d19a9e3841467b7d70fe9737542f187eda48cd0e5c62
+size 7191
