dc7fd5892a78d82a627fb809d1e596b102819cd4
진아영 <ay8160@daum.net>
2024-11-09 15:46:52 +0900

    feat: 장바구니 로그 전송
    Former-commit-id: cdec2f9eb4574623d3f0c64320c2d68c3f870be5

---
+++ frontend/app/odd/lib/apis/cart_api.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:541839c23089b7d6b72dd669745b02df5c3a6388155e35e50c45210009e8e334
+size 1201
--- frontend/app/odd/lib/modules/cart/controller/cart_controller.dart
+++ frontend/app/odd/lib/modules/cart/controller/cart_controller.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd7b52d5894a660db5ea691ecf4843e7e5429b6d3aeb66068704a85e5b758845
-size 3890
+oid sha256:ca8a709ffdcb1f04e41140269dc0105859f85437450e9ac60d0fc81c6a848548
+size 4179
