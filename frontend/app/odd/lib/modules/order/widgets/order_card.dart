617afd5a409280361e1b34f917a6d0b3ffc06a7d
c4526 <c4526@naver.com>
2024-11-09 17:07:52 +0900

    feat: 주문 상세 페이지 UI 제작
    Former-commit-id: c7e507edfedae0124f3dd0f8528454a415683a46

--- frontend/app/odd/lib/modules/order/widgets/order_card.dart
+++ frontend/app/odd/lib/modules/order/widgets/order_card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84a1927806739a7a87ec6624d435cfee5af3711f26c715de38d717a76e587477
-size 4914
+oid sha256:b7754be80de52c03f1c3e3bdb68c5dea2a2419c37e7534f7413a45e14b86b5ca
+size 5492
