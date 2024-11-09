d35b00cfeb8800574b16125129bfe3019827c267
이승민 <tmdals77117@naver.com>
2024-11-09 16:54:05 +0900

    feat: 결제 모달 포인트 연결
    Former-commit-id: f050a19b64f57cf4cdf7f992edd713ecb4a995e1

--- frontend/app/odd/lib/modules/pay/widgets/pay_modal.dart
+++ frontend/app/odd/lib/modules/pay/widgets/pay_modal.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37ff6242fb845c9b8f97f8c78a03484a86d9aeb3592cd0f08517f4a68c2bffd0
-size 1328
+oid sha256:9afb4f34cd70e1f75d6907047a201574ab4c33adae76bbd9a397984487f7ea40
+size 1595
