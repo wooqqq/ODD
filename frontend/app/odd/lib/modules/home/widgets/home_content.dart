ae0b6d117b87e859daa74a0202651c745d5c2c69
진아영 <ay8160@daum.net>
2024-11-10 19:36:51 +0900

    refactor: 더미데이터로 변경
    Former-commit-id: 4abb40b691a57b9867dbbff01a7872547a1247cc

--- frontend/app/odd/lib/modules/home/widgets/home_content.dart
+++ frontend/app/odd/lib/modules/home/widgets/home_content.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7dc006438b0dcf1124fc23ac291966d33251411d6c3f4c519310e15fd8db2c1
-size 6952
+oid sha256:c5240865b5390a7f95cb635adf2e8dcbcccd6f83d8845417f863971f5ec06422
+size 5911
--- frontend/app/odd/lib/modules/home/widgets/platform_content.dart
+++ frontend/app/odd/lib/modules/home/widgets/platform_content.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d8d63e0752b1afaad99f5b9410d77bfdeb916e29612a4d00f103c51e2ada216
-size 7566
+oid sha256:3880b2e4df27268a3ae5e050f124e139ec40202523f768ab49874fb14eaa5f8f
+size 6005
