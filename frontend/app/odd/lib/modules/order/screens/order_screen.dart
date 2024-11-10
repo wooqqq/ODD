c132716b5d82e6568e8bafe300fe1794b8de6abb
c4526 <c4526@naver.com>
2024-11-10 20:51:59 +0900

    refactor: env 캐시 삭제
    Former-commit-id: c36b09159c47c74230f028cf9df1d8c36cef7f5c

--- frontend/app/odd/lib/modules/order/screens/order_screen.dart
+++ frontend/app/odd/lib/modules/order/screens/order_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e949dcc0039b346756723f5cf32764df239e1c6e968d7b6f3804a3842a730c0
-size 4375
+oid sha256:cd0cefabed3ad37e67f2de58c3fd5ceee0d5aac819680a41368c549a61a6b065
+size 4853
