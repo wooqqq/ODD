e54f56c82ede5c938d51ea467134a8ac8816d597
최승탁 <tmdxkr5@hanmail.net>
2024-11-10 14:35:54 +0900

    feat: PurchaseItem 및 PurchaseLog table 변경
    Former-commit-id: 67e4b2892b28685de9a32ddf48476956f7e6dae3

--- backend/client/src/main/java/odd/client/common/purchase/service/PurchaseService.java
+++ backend/client/src/main/java/odd/client/common/purchase/service/PurchaseService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7f620e0218c88e0197e3f5e8088f3d771da5402a6707dcd027df042d0370024
-size 2503
+oid sha256:84afe92f937aeb8319cc25a8deea48c24743ab70592d1f8dd8aac7a70514629e
+size 3006
