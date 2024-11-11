8706575a9e52b8709ce7ea7a93422a240b06598d
c4526 <c4526@naver.com>
2024-11-11 23:38:56 +0900

    feat: 주문 상세 api 연결
    Former-commit-id: 86af3e63fdec6fea2a98a7c9169b721778b9e3cb

--- frontend/app/odd/lib/modules/order/widgets/order_card.dart
+++ frontend/app/odd/lib/modules/order/widgets/order_card.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:024093d6da8b3e119281e30a97bfd57700a526570c687b2e13d56796157792f9
-size 5689
+oid sha256:99b80ad4c26eaf9dcbba2990c241484f641c8affb0e5de502ab80b3002e46aa8
+size 5804
