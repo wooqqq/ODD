f498cd4f459e7e60a4ea0f0e6b416a67532de699
진아영 <ay8160@daum.net>
2024-11-11 09:46:37 +0900

    feat: gitignore 추가
    Former-commit-id: 3db22eb03e3a7dfc390f6f0fb413ef72346fc822

--- frontend/app/odd/lib/main.dart
+++ frontend/app/odd/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64135122ca675b4f11874e4f5c6878bc29a826f948f26dd2f4126944a4c726da
-size 2490
+oid sha256:00127decfe9e8b9ce4740d64147fc9fbfd6f926c15a931bbd213cf44b974552c
+size 2460
