88a25d52c928dadc495487beb5abd9ae6ba34647
이승민 <tmdals77117@naver.com>
2024-11-11 13:05:29 +0900

    design: 앱 아이콘 및 이름 설정 / Fe 230 home
    Former-commit-id: 6deb0b9fefeb601757d2a3fdfead09e48812c7e3

--- frontend/app/odd/lib/modules/home/screens/home_screen.dart
+++ frontend/app/odd/lib/modules/home/screens/home_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1313d19de240ec46a103c015de81f7c81488648980e726f028db70ec835e0e5
-size 3754
+oid sha256:054f330ad5c59a8f2a477e98db17404a4c5f666b05bcd1236fb6465cf5983067
+size 3850
