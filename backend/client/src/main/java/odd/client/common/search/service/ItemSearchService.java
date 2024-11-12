4793b59bc6c1b9d3662f0074c9b006be993cd19a
이소민 <s.min.s9098@gmail.com>
2024-11-12 15:49:47 +0900

    feat: 검색 결과 Response Body 수정
    Former-commit-id: bd3302fffa7c6f910192e8aa45336242d72e5d22

--- backend/client/src/main/java/odd/client/common/search/service/ItemSearchService.java
+++ backend/client/src/main/java/odd/client/common/search/service/ItemSearchService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1e95d3e1f8721044f9dfa4b77e65891a227879ec4d5b2bb59ddeb0cc220e7eb
-size 5599
+oid sha256:a5c2624e118863f030952fb85385fc12423977edf753c14964affb89c0c05db4
+size 5953
