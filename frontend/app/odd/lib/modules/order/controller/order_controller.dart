2aa45d74b7b3dc2358c8ddb247f07a6b0689f9fc
c4526 <c4526@naver.com>
2024-11-12 16:25:59 +0900

    fix: 필터링 수정
    Former-commit-id: 422e403cbe6af765459607dc0460a5c3d98f93d2

--- frontend/app/odd/lib/modules/order/controller/order_controller.dart
+++ frontend/app/odd/lib/modules/order/controller/order_controller.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac6eedb1b4f6336b0bac0dc069d189805f8de0b64655844725d300d57f1b621f
-size 4613
+oid sha256:2a7403bb980c5c8121dd2befe942ebf7cf87011b011563827ea134e6f117cc1a
+size 4459
--- frontend/app/odd/lib/modules/order/screens/order_screen.dart
+++ frontend/app/odd/lib/modules/order/screens/order_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a6d1fc9c6ee0f2c628bf2d459f9339519457437fd37a2e89f704f7c75d7cfc9
-size 5520
+oid sha256:277e2e729d6358fa84ad1ae9d1e8a2e34c4d8dc8130cacedaedd835525e313dc
+size 5370
