3f8a6ec6c60845e93e72d8d0140e46067c62ce64
진아영 <ay8160@daum.net>
2024-11-12 16:12:01 +0900

    feat: 플랫폼별 컨트롤러 설정
    Former-commit-id: 6c81c032b849c0cdd77a9e5512c158e341663dd8

--- frontend/app/odd/lib/modules/home/controller/recommend_controller.dart
+++ frontend/app/odd/lib/modules/home/controller/recommend_controller.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7c7bf0eed75f6b3acebdf15f14871d0c4b0a3c034774b4fcbb7f5e47d7cbd54
-size 6048
+oid sha256:4e01be550969b2b8a02cee34ddf3b5ea00a13500640dc6328f2619e847f4b2cb
+size 6006
---
+++ frontend/app/odd/lib/modules/home/widgets/gsfresh_content.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:76d48949277c4c0504e58634077d382cf5610782e8588aa643f837ebece421c9
+size 7182
