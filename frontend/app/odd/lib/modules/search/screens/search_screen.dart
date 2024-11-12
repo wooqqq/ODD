aafb53c3cb8bdf180f32ea8ad8db2e9f2bb11272
이승민 <tmdals77117@naver.com>
2024-11-12 16:20:09 +0900

    feat: 검색 자동완성 기능 / Fe 509 search
    Former-commit-id: c7463f4f2f06a02dc5bc3507636711c11d116b30

--- frontend/app/odd/lib/modules/search/screens/search_screen.dart
+++ frontend/app/odd/lib/modules/search/screens/search_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42aa3b697ab8924be8ff5509ef86f409ae85a66f70ef9c4f39f3f24254067a25
-size 3291
+oid sha256:79f7908dd93193d5d2f0491801e68bd71bab2aad3aa8fccb086dde4bc416299a
+size 5219
