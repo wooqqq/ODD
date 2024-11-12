cb09bbd962a0d5cbb01d8e3ef276a607a8fb2c59
wooqqq <sge0225@naver.com>
2024-11-12 12:45:55 +0900

    refactor: Redis 직렬화 방식 수정
    Former-commit-id: ad5e1b58f7ddb0c094682286da4928539b20ab14

--- backend/client/src/main/java/odd/client/global/config/RedisConfig.java
+++ backend/client/src/main/java/odd/client/global/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1764f181365999a5829e50cfbc027cafdb4afceb40e65f66f488845eaba1e85e
-size 989
+oid sha256:f99d6547f169831a2cda8f7ab149c6d2db6e3053be7e44807ae6876cc979de63
+size 1392
