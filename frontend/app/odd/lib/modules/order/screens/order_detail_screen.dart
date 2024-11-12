9aa23d29732aced498a3ab15c5a197d6a60eaded
c4526 <c4526@naver.com>
2024-11-12 22:07:08 +0900

    refactor: 주문 내역 버튼 수정
    Former-commit-id: f4b366982435574af4ac6cdf4519c4ced0200f6d

--- frontend/app/odd/lib/modules/order/screens/order_detail_screen.dart
+++ frontend/app/odd/lib/modules/order/screens/order_detail_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6179778b37200303ddc1b33629cca0936bfed6843dec417dfd338f8e28576abc
-size 10576
+oid sha256:5e38b71bdb8537a142e264397b4fa6c57eca7140849ea900e28a6d083321a170
+size 10575
