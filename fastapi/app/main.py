0a6becd22691ed713bd90784c3d97764d6909413
wooqqq <sge0225@naver.com>
2024-11-12 15:55:09 +0900

    fix: 데이터 DB에서 불러오도록 수정
    Former-commit-id: 2a4b632cb567b894b4989c646e3fecf896562c93

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7dd44b2fc1395ebee7dfdb48e6b45d8c0fa0028cc30fae92c1bcb4fb10f9ee9
-size 1000
+oid sha256:06945daac26d704b326d526328645395d6723c966dc0aa80617ba6df7493ff43
+size 1148
