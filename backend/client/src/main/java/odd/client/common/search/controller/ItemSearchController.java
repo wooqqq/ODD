527d0e76ca946e71ce3ea9fcc8afdab1e36c60b3
이소민 <s.min.s9098@gmail.com>
2024-11-12 13:23:32 +0900

    feat: 검색 글자,단어 포함 검색 및 자동완성
    Former-commit-id: 1dfde302e2b701cd68ead5a64cf92dc2ec243ebe

--- backend/client/src/main/java/odd/client/common/search/controller/ItemSearchController.java
+++ backend/client/src/main/java/odd/client/common/search/controller/ItemSearchController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83f944be2626dbd1df653b6885857ae7ebf7d921c446ad738994c54cc6052ad8
-size 1480
+oid sha256:428af504e3b438e9a8ce15a21acbd6163ba6374b802cfe6c20096d0387d9e416
+size 1402
