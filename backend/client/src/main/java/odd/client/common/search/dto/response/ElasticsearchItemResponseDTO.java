5a7e378ad8544f1a884223133cecd5b400549d6f
이소민 <s.min.s9098@gmail.com>
2024-11-13 22:16:55 +0900

    feat: id 값을 string으로 변경
    Former-commit-id: 1e8008dde676805b6140bcd182b580e37a614d5f

--- backend/client/src/main/java/odd/client/common/search/dto/response/ElasticsearchItemResponseDTO.java
+++ backend/client/src/main/java/odd/client/common/search/dto/response/ElasticsearchItemResponseDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:999292b3c76152f224fd4c2c939aa23f60dfa0cb9898c76f4986f91ae1e1b27e
-size 786
+oid sha256:783d58043237f6931a793c0b7bf47ab4cfb9d2bca83934ddee6621c7139bd4be
+size 788
--- backend/client/src/main/java/odd/client/common/search/service/ItemSearchService.java
+++ backend/client/src/main/java/odd/client/common/search/service/ItemSearchService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be0e8f68e927d341eef160e8cc44662ded2605ed287a7fef8735e4c50765acab
-size 7715
+oid sha256:8ec74cf0c31012a62d8e95ce91a4154615a996e4b75cb3a7f31b28f902037ef9
+size 7674
