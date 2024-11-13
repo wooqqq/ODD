81ca0c0da8b950933ecb2010f7e687792b66a5d1
진아영 <ay8160@daum.net>
2024-11-13 22:54:56 +0900

    fix: 데이터 구조 변경
    Former-commit-id: 82c49cb9f9c03bcbc06c88f1d36097d762724129

--- frontend/app/odd/lib/modules/home/widgets/category_button.dart
+++ frontend/app/odd/lib/modules/home/widgets/category_button.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70af9823f4128ad4e5568b9d65a053a5cca7cd5758f7e30a2423d7d0cd2e9269
-size 1483
+oid sha256:880dee5a0bec3aa87b2d5a2c563e01ba87d81000b4374669aab10ffcf35a3619
+size 1464
