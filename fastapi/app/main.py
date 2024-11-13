ffb4316a529b5363653c0957eeec41e2ceea3aa4
wooqqq <sge0225@naver.com>
2024-11-13 21:52:27 +0900

    fix: cassandra 연결 테스트 완료 및 수정
    Former-commit-id: 8e1e7bf41d1460e314f5b3dbb4964fb3752977d5

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91db8097213c5e0f38d84a19dc5d18bde08dbf65e701e7b3056ed300f6c9d9a8
-size 1357
+oid sha256:c3477254ffae2e785394bb7cec9c46a01c7c7b47bd76fda324ef3edb1b2772de
+size 1395
