4534c886f8c5f63e8c12bbefad29770ff181c0ec
진아영 <ay8160@daum.net>
2024-11-14 20:05:32 +0900

    feat: 알림 이미지 변경
    Former-commit-id: e0e72405f41bff4d2535d5b69c2ad04f9eedd6fc

--- frontend/app/odd/lib/modules/notification/screens/notification_screen.dart
+++ frontend/app/odd/lib/modules/notification/screens/notification_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49d019b68e455b84315b68469c03d45073f3ae35a7b1cc64960bbfff3ecd34b8
-size 3016
+oid sha256:c80d936b5a65993c8a1fc6c294a5c5e54bf33326647fcc60149bb52bcc28031a
+size 2935
