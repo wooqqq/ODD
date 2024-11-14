0eb960da0301b06a2bb2dd78fddaec0401809e5b
진아영 <ay8160@daum.net>
2024-11-14 21:26:32 +0900

    fix: 정렬 수정
    Former-commit-id: 98c18873376fc345943ac86f323ed1f260137210

--- frontend/app/odd/lib/modules/home/widgets/wine25_content.dart
+++ frontend/app/odd/lib/modules/home/widgets/wine25_content.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a12462ec673ffbd8c14d23ba80b521ac6526aa4abdeb6a8d53e4446a2950bdd9
-size 2847
+oid sha256:39fd3c2ce0ad01dc82c3f055b6a348b377f7e942287e7dfc9c225de473482af3
+size 3092
