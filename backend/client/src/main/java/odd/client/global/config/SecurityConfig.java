99cc5f0e7ae784d9c884f5a9ea16cb4101c31af6
최승탁 <tmdxkr5@hanmail.net>
2024-11-15 08:36:18 +0900

    feat: 정성평가 hard 코딩 구현
    Former-commit-id: 4ef408242a9ac459bf05e9a7ecfb2ba5011b6195

--- backend/client/src/main/java/odd/client/global/config/SecurityConfig.java
+++ backend/client/src/main/java/odd/client/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:050862e7ab9bad7ed780a55d326b64c2e5efa57b37b6693c4441a9f1e5085997
-size 2633
+oid sha256:f8a6d51774ba03f8c0f47dd242dab4960446468ba0edff9f730eaa6864799d55
+size 2666
