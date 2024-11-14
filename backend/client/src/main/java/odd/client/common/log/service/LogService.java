d12554a425949077a692f99199e9a32ff834dfb1
최승탁 <tmdxkr5@hanmail.net>
2024-11-14 22:09:42 +0900

    refactor: cart platform, servicetype 에러 수정
    Former-commit-id: c63b64ff82c68e4ca3a1e17a5a25ceb1da1d0b2b

--- backend/client/src/main/java/odd/client/common/log/service/LogService.java
+++ backend/client/src/main/java/odd/client/common/log/service/LogService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab36de1df0824c3f2565803de9cc49129a6189a557e80ed2b97a4c7eaaf7d334
-size 5649
+oid sha256:4f48ae2cdf0472f73ff61f2a806472da692de1fa388a161047a322e19f86480b
+size 5872
