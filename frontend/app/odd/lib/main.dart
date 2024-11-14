1b30767f45d2838046367a20c4f367b9702b7e1d
c4526 <c4526@naver.com>
2024-11-14 15:27:24 +0900

    fix: 카드 모달 수정
    Former-commit-id: c94216547363495686ed09cc2ddb884f0f64b2fd

--- frontend/app/odd/lib/main.dart
+++ frontend/app/odd/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:036395d301f2ac8fc8f9afb51bcd9b506d1b6e1901f56ce517f3333cb4a43cd6
-size 2450
+oid sha256:71a17865f86f86ea557252af7909a398f6f518a4f7cd07ee8fa7143852f9f764
+size 2449
