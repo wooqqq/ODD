e21624c2625425e7941f914290a20da4c44d6f29
진아영 <ay8160@daum.net>
2024-11-14 12:39:27 +0900

    feat: 홈화면 추천 리스트 슬라이싱
    Former-commit-id: b55196dd94ef91de7671c8012d4a57cb53f23c37

--- frontend/app/odd/lib/main.dart
+++ frontend/app/odd/lib/main.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:036395d301f2ac8fc8f9afb51bcd9b506d1b6e1901f56ce517f3333cb4a43cd6
-size 2450
+oid sha256:3e8076c5b9f1e5d8331c6585d1edca4e88f1346bc7aec4060d85ba7f61a42d36
+size 2559
--- frontend/app/odd/lib/modules/order/screens/order_detail_screen.dart
+++ frontend/app/odd/lib/modules/order/screens/order_detail_screen.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:387205a05738f438da0ead8ffdc03a33837eeb14413200d0304ad5fade6efddf
-size 10699
+oid sha256:7153a77d742e5ca56e16604c3c47626418616b3e55585caba2ef2db688c2ecb5
+size 10652
