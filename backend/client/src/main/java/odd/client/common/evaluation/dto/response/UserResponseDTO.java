5a44ff66eb2b2abce021e371f0886e6f7a279eba
최승탁 <tmdxkr5@hanmail.net>
2024-11-15 21:11:14 +0900

    feat: UserResponseDTO 직렬화 적용
    Former-commit-id: 9153cf5f8cb66e61d5c6820267551cbe172565a9

--- backend/client/src/main/java/odd/client/common/evaluation/dto/response/UserResponseDTO.java
+++ backend/client/src/main/java/odd/client/common/evaluation/dto/response/UserResponseDTO.java
@@ -1,11 +1,17 @@
 package odd.client.common.evaluation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponseDTO {
     private String userId;
     private String nickname;
     private int age;
     private String gender;
+
+    public Integer getAge() {
+        return age == 0 ? null : age;
+    }
 }
