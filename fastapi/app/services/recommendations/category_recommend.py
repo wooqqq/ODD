1520a9ce2459b2a6e3413737c0345b4c55ffb571
이소민 <s.min.s9098@gmail.com>
2024-11-16 21:18:34 +0900

    feat: 서비스 완성
    Former-commit-id: 701061d8ec97d0b4189dc314e3bc6036ebb056d5

--- fastapi/app/services/recommendations/category_recommend.py
+++ fastapi/app/services/recommendations/category_recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a47be9fd6cf251af801b650c4d0e46835176949d4de3145c0de21cf06f71c201
-size 12631
+oid sha256:fed24fa632a7e86997f4a599c9b88a85d76a8c9e271697420242bcd1ad07bd73
+size 13150
