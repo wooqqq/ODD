a736ccb7933ff9d0b5e8ca0353279e16a13229b9
최승탁 <tmdxkr5@hanmail.net>
2024-11-16 22:44:34 +0900

    feat: 최근 구매 상품 조회 최근 순으로 정렬
    Former-commit-id: c97eac42cba27553e4854277344d261f41c396e3

--- backend/client/src/main/java/odd/client/common/item/service/ItemService.java
+++ backend/client/src/main/java/odd/client/common/item/service/ItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98db8e366e7e0ced7f1f37292f12c563d77498f1730dcf3c7e988f2be6c6238d
-size 20193
+oid sha256:972e19bc428130561d79b7b700db12b224443520861d8b194343c1ead0fad7b8
+size 20140
