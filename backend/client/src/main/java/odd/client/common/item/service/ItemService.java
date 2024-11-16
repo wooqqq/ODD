fbda912c042eed4cc123e6e39bbfdc4d35c032b8
이소민 <s.min.s9098@gmail.com>
2024-11-16 21:40:56 +0900

    feat: 카테고리 추천
    Former-commit-id: 8171d9e3d1b0fe702b49b4a9a7793d5bb83b5e71

--- backend/client/src/main/java/odd/client/common/item/service/ItemService.java
+++ backend/client/src/main/java/odd/client/common/item/service/ItemService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14161466a7bba4166872b9735956976cc9f54f5aa7f5c42a6b5597ea4663e480
-size 17226
+oid sha256:cfbbb84d328cf60377784cce09e9d46e852129619faee6d36245535f4d8fd2d5
+size 17401
