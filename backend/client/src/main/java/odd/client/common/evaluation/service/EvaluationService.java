4cfa0f6d8595a34df7769eedaf4f5a7d99b95bb2
최승탁 <tmdxkr5@hanmail.net>
2024-11-16 15:45:57 +0900

    feat: 정성평가 기본정보 구현
    Former-commit-id: 6ee5aefdf5e16cc6c0b4dcf88f25ff6bcbb294b3

--- backend/client/src/main/java/odd/client/common/evaluation/service/EvaluationService.java
+++ backend/client/src/main/java/odd/client/common/evaluation/service/EvaluationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80003f889057f1d73127cb4d76ff14633ed39b95dab483025ce220da10ffcde2
-size 5438
+oid sha256:af2ea6d9956cb7285c6889f36ec7bcd733e73525e43737c3d169163116ee7fa7
+size 5816
