a050a0db2e82c3690925febd6bea1a1a7d78ed08
wooqqq <sge0225@naver.com>
2024-11-17 17:21:32 +0900

    feat: 시간별 알림 전송 기능 구현
    Former-commit-id: 6861505ff581feba84b1e6fe5822cb7f96624a58

--- backend/client/src/main/java/odd/client/common/notification/service/NotificationService.java
+++ backend/client/src/main/java/odd/client/common/notification/service/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60d81ad38e2a0102ed08bdb8fd933dd1aad6c178e8bdf8d3173ae44aac5b1c5d
-size 4110
+oid sha256:912ea454fcabd4c966551a0b01b60e4cb57eabe28b5de59dd764f7c8dd8f12c8
+size 6364
