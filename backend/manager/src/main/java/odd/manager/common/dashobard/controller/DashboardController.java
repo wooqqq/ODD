a293810bc99423400273d9cd5ef09e61c748a6dc
wooqqq <sge0225@naver.com>
2024-11-18 01:51:18 +0900

    refactor: DashboardController path 일부 수정
    Former-commit-id: 5c709c94967f65a4bfd1e109335a0d0079c99601

--- backend/manager/src/main/java/odd/manager/common/dashobard/controller/DashboardController.java
+++ backend/manager/src/main/java/odd/manager/common/dashobard/controller/DashboardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7b5858a8ff8cd8d0c038997722abae67a82a5ff52441f24e1a48c53a2eabaad
-size 3110
+oid sha256:00fa4ff5e8e2129a7b107bc64edd8d9f65f780cf67d3b16c5bd5b8b5812dccb2
+size 3135
--- backend/manager/src/main/java/odd/manager/common/dashobard/service/DashboardService.java
+++ backend/manager/src/main/java/odd/manager/common/dashobard/service/DashboardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:099e67e77b6fc5804f88c259ea548cec9e5114b1edf0ea14b51bb370854fe059
-size 4051
+oid sha256:621a0cf97d37fa37f8dd75c34f6d7e3907623d18fee29b59e506258d44e58417
+size 4076
