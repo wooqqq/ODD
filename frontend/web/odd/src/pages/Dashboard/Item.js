8b8740045b0033ec278167078b8129621377ca78
이승민 <tmdals77117@naver.com>
2024-11-17 23:47:30 +0900

    Fe 244 dashitem
    Former-commit-id: 1493301467b7cb67bf644f6ea2f2e07e695909c8

--- frontend/web/odd/src/pages/Dashboard/Item.js
+++ frontend/web/odd/src/pages/Dashboard/Item.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c68a7717bb37f875945e2689506084a98b34a7cca283ab2734afc57420d8f765
-size 1977
+oid sha256:9d03dcf1b0a1692cdb537dbc397837518f5525faca07b6f4bf7b089ae2d5a7cf
+size 2239
