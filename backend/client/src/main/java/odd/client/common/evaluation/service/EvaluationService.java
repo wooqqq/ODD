726358a119deb693cfbb4a974c8f41225f019623
wooqqq <sge0225@naver.com>
2024-11-18 02:58:06 +0900

    remove: 필요없는 코드 제거
    Former-commit-id: f4cbb62f9e6c7cae64ea6ce90674c562a5adef8e

--- backend/client/src/main/java/odd/client/common/evaluation/service/EvaluationService.java
+++ backend/client/src/main/java/odd/client/common/evaluation/service/EvaluationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea7c0c875884ffdcb75999923a47ad3f8bd2c2622e670db460f8ff2944ff32de
-size 15565
+oid sha256:97931b4a25c80f0a930b0b3e5850bb5acd4b36b332c8845f08240faab4756d5a
+size 15287
