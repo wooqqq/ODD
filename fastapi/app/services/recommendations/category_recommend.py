0d3c34cffff82ef7e85effc7a0229d02bb12e3ba
이소민 <s.min.s9098@gmail.com>
2024-11-17 16:35:46 +0900

    feat: 서비스타입 변경
    Former-commit-id: 93cd3b32c12b210448926ed87832e83e99f3bac0

--- fastapi/app/services/recommendations/category_recommend.py
+++ fastapi/app/services/recommendations/category_recommend.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fc4c56b80cf3ac4b7c737fab010c2c3d6a5ef1c1ad91be4951dad929cd9a1cd
-size 13501
+oid sha256:9c606c956d3a02b1b0302ee9b1721e236f029475526c20dce17c71116cfa532e
+size 13345
