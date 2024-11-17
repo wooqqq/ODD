f1a697499c0870591f3dca990591365ad9f01926
wooqqq <sge0225@naver.com>
2024-11-17 15:53:05 +0900

    feat: 시간대별 재구매 추천 알고리즘 구현
    Former-commit-id: 874f040af40c42066bb45a2ecc854f8bda5e2b07

--- fastapi/app/models.py
+++ fastapi/app/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33f201df295cc2c7a8136c58dd666d5976b6b302b8403704d20f04d4a831139c
-size 1155
+oid sha256:e544b4eb8118c228ec6de55c846eeef15077966a555aa78d657207b5a2fc6897
+size 1340
