2212eeb18cac54e8ff97d96dcff2c36cbd43ed3c
진아영 <ay8160@daum.net>
2024-11-18 09:08:20 +0900

    feat: 로그인 설정 추가
    Former-commit-id: 7f5153134cafb0241deadf54d59a13f59de0e1c5

--- frontend/web/odd/src/App.js
+++ frontend/web/odd/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1854707edd13c1ddc5ec09d93c80739b7ed88879e3b7a3ebf66ed94a47e9a6de
-size 1657
+oid sha256:daff21d9778857d2e6b886f6fbb2a6924c61a8b47508940bab0bff1c2e0093c9
+size 1725
--- frontend/web/odd/src/stores/adminStore.js
+++ frontend/web/odd/src/stores/adminStore.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51226bd1a0da7aef5e37739d1dc4a5e35538428ffef8e39a14d905e449134abe
-size 528
+oid sha256:52d4b206dd644fb5c7c826baffb24bcfeb803aaaa3bff4403e22b32e038f3d88
+size 595
