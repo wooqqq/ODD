03069a78be69d65296c1e9e0fefeefef81710173
wooqqq <sge0225@naver.com>
2024-11-18 21:13:18 +0900

    refactor: 재구매 사용자 수 관련 리팩토링
    Former-commit-id: 75b296143ea2601d5e0bba54f141c83cbbb72cde

--- backend/client/src/main/java/odd/client/common/dashboard/service/DashboardService.java
+++ backend/client/src/main/java/odd/client/common/dashboard/service/DashboardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c852181c91f9f195fb516f37c5c1e121ea473481d45212a79771c7b9e123088c
-size 20223
+oid sha256:b2dcb976b988c7b036b0a5af06bb6a011156d261f6ced11159b476c676ede16e
+size 21051
