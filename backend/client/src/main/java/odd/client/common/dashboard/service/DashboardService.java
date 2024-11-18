cbe20f08df6231ce5503d5dbfc916af43e19c37f
wooqqq <sge0225@naver.com>
2024-11-19 01:24:21 +0900

    refactor: 상품 수 조회 리팩토링
    Former-commit-id: 1a90ec97ba96fd4510c25fe2613034a130ac20e0

--- backend/client/src/main/java/odd/client/common/dashboard/service/DashboardService.java
+++ backend/client/src/main/java/odd/client/common/dashboard/service/DashboardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b84f85fb67c9f6c5fae749cc36e045dd9a80ccf33c1b8cfbc60d8591b0455a4
-size 22781
+oid sha256:59219e4eaac1b44b40f35abcbaa31e62e4e3c90c371a297b1b80acc78af8ce03
+size 22706
