bad717633340c5765d79b379abf5ec7b40fe82d9
wooqqq <sge0225@naver.com>
2024-11-18 22:58:04 +0900

    refactor: 알림 멘트 수정
    Former-commit-id: 4078b5cb17f15be44936b82184338b39d092faee

--- backend/client/src/main/java/odd/client/common/notification/dto/response/PeriodNotificationDTO.java
+++ backend/client/src/main/java/odd/client/common/notification/dto/response/PeriodNotificationDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc1c3741df708c1c47de367a6e1361492a3956d54c376b16dc0465643a07d8f7
-size 1137
+oid sha256:179e9f90ae59758e6cd99c9c09d49709b26ae9fbdd9d9578f38cddddfb7e4fb2
+size 1074
