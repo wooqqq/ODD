9a984e0ad4eca01e469bf627f024df6fa42a254c
wooqqq <sge0225@naver.com>
2024-11-19 01:36:43 +0900

    refactor: 카트 수 리팩토링
    Former-commit-id: 730b4283c0ca2c228b8a6f3190eec9b6f3c89a43

--- backend/client/src/main/java/odd/client/common/dashboard/service/DashboardService.java
+++ backend/client/src/main/java/odd/client/common/dashboard/service/DashboardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a64d8310c01c7e0764a4214742a30703c35a4e8757edc691f00e73bdc35fd8b0
-size 22758
+oid sha256:ba34c3bad56e81cffd36cbc769ae1ca03853a6157d745ac36c4c2f2c28580f03
+size 22037
