af697f826b7851baa9dd33f5ca4ca49d09047dd2
wooqqq <sge0225@naver.com>
2024-11-18 09:42:41 +0900

    feat: 주기별 재구매 추천 상품 조회 기능 manager 와 연결
    Former-commit-id: 205698f7a9f888278846592fbdf304ab14e09ce1

--- backend/manager/src/main/java/odd/manager/common/evaluation/service/EvaluationService.java
+++ backend/manager/src/main/java/odd/manager/common/evaluation/service/EvaluationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54ea408fe1d2c5bb672c054774808d760c8d5ce773906c83fbcdc1d2022f12a1
-size 3171
+oid sha256:1c7566725e7cf8de44d28916820cedd3b8fa77c9d141312069ee0b4292ae6950
+size 3177
