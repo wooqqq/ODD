bc16a55efdcda3938f3a0a5940608f7b7265f531
wooqqq <sge0225@naver.com>
2024-11-18 20:43:48 +0900

    refactor: Notification 저장 폼 수정 및 data 내 넣는 정보 추가
    Former-commit-id: ca7199c7c5d2e54864991ee3cef3af27f16a215c

--- backend/client/src/main/java/odd/client/common/notification/service/NotificationMonitoringService.java
+++ backend/client/src/main/java/odd/client/common/notification/service/NotificationMonitoringService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f253a5241044d280e6fbb4d15e1db348d770c3519d9452daac4e239f619997e
-size 5419
+oid sha256:a8487ad161c7c299e899b56c5af47cc4680508b131bb20c830e090110de58c8f
+size 5417
--- backend/client/src/main/java/odd/client/common/notification/service/NotificationService.java
+++ backend/client/src/main/java/odd/client/common/notification/service/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:912ea454fcabd4c966551a0b01b60e4cb57eabe28b5de59dd764f7c8dd8f12c8
-size 6364
+oid sha256:4d31dafc65d1413ac8b69307bae7970d8285e6763fd3ac12605eda258459f423
+size 6453
