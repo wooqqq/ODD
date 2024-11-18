297e1e6af203d401f5ff4da1fc8ac3cd3dff26cc
wooqqq <sge0225@naver.com>
2024-11-19 01:30:53 +0900

    refactor: 상품 조회 수에 따른 정렬 기능 구현
    Former-commit-id: b486c44083ecfcc1c086e4faa5ed35bce3ce340f

--- backend/client/src/main/java/odd/client/common/dashboard/service/DashboardService.java
+++ backend/client/src/main/java/odd/client/common/dashboard/service/DashboardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59219e4eaac1b44b40f35abcbaa31e62e4e3c90c371a297b1b80acc78af8ce03
-size 22706
+oid sha256:a64d8310c01c7e0764a4214742a30703c35a4e8757edc691f00e73bdc35fd8b0
+size 22758
